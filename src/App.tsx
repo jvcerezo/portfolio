414cd12f5ecd5dd55b1533314f3f9870b7063590
jvcerezo <jvcerezo@up.edu.ph>
2025-08-31 02:52:26 +0800

    applied possible deployment fix

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,6 @@ import { Card, CardContent, CardDescription, CardHeader, CardTitle } from './com
 import { Button } from './components/ui/button';
 import { Badge } from './components/ui/badge';
 import { Separator } from './components/ui/separator';
-import { Avatar, AvatarFallback, AvatarImage } from './components/ui/avatar';
 import { Tabs, TabsContent, TabsList, TabsTrigger } from './components/ui/tabs';
 
 function App() {
